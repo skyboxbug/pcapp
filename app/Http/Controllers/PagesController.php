f0c03a21019fbc3b87f2acaed447fc3657170f19
Owen <lookhire@gmail.com>
2017-12-09 01:18:10 +0700

    added more stuff

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function home(){
+    	$title = 'Home';
+    	return view('pages.home')-> with('title', $title);
+    }
+
+    public function about(){
+    	$title = 'About';
+    	return view('pages.about')-> with('title', $title);
+    }
+    public function cpu(){
+    	$title = 'Pick a CPU';
+    	return view('pages.cpu')-> with('title', $title);
+    }
+    public function admin(){
+    	$title = 'Admin Page';
+    	return view('pages.admin')-> with('title', $title);
+    }
+}
---
+++ resources/views/pages/admin.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<h1>Admin Page</h1>
+	<form class="form-inline my-2 my-lg-0">
+        <input class="form-control" type="text" placeholder="Find Parts">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>	
+	
+@endsection
